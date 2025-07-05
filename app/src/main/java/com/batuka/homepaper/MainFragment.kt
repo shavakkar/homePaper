2e9e77fefed6b4f239420a921a8b9ad567dc2c4b
shavakkar <shavakkar@gmail.com>
2025-07-05 16:16:36 +0530

    changed button

--- app/src/main/java/com/batuka/homepaper/MainFragment.kt
+++ app/src/main/java/com/batuka/homepaper/MainFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainFragment : Fragment() {
     override fun onCreateView(
@@ -14,7 +14,8 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
-        view.findViewById<Button>(R.id.btnGoToImages).setOnClickListener {
+        view.findViewById<FloatingActionButton>(R.id.btnAdd).setOnClickListener {
+            // Navigate to Images fragment or perform your action
             findNavController().navigate(R.id.action_mainFragment_to_imagesFragment)
         }
         return view
